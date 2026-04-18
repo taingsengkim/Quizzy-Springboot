ac3fc9af0b422cc86a8c9b10e47337decbf21357
unknown <mrrsaki0403@gmail.com>
2026-04-19 03:09:57 +0700

    CORS

--- src/main/java/co/istad/y2/quizzy/config/CorsConfig.java
+++ src/main/java/co/istad/y2/quizzy/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://quizzy-frontend-41e1.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
